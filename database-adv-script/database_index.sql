cf46d843757eea4aa73e4ea373977b0cc0a5e544
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-12-01 18:42:28 +0200

    Identify and create indexes to improve query performance.

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -9,4 +9,16 @@ CREATE INDEX idx_property_host ON Property(hostId);
 CREATE INDEX idx_property_location ON Property(location);
 CREATE INDEX idx_booking_property ON Booking(propertyId);
 CREATE INDEX idx_booking_user ON Booking(userId);
+CREATE INDEX idx_booking_status ON Booking(status);
 CREATE INDEX idx_payment_booking ON Payment(bookingId);
+
+
+EXPLAIN SELECT * FROM Booking b
+JOIN Property p ON b.property_id = p.property_id
+JOIN User u ON b.user_id = u.user_id
+WHERE b.status = 'confirmed';
+
+ANALYZE SELECT * FROM Booking b
+JOIN Property p ON b.property_id = p.property_id
+JOIN User u ON b.user_id = u.user_id
+WHERE b.status = 'confirmed';
