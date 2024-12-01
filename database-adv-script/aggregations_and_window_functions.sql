05ea8ec2c10d129d845ecf6d611535e0142a3e9b
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-12-01 17:20:35 +0200

    Apply Aggregations and Window Functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -11,7 +11,8 @@ GROUP BY u.id;
 
 SELECT p.id, p.title, p.location,
     COUNT(b.id) As total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.id) DESC) As booking_rank
+    RANK() OVER (ORDER BY COUNT(b.id) DESC) As booking_rank,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS booking_row_number
 FROM Property p 
 LEFT JOIN Booking b ON p.id = b.propertyId
 GROUP BY p.id
