64485c5ca75ab9d823596d8418104dad21e0f55b
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-12-01 18:48:34 +0200

    Identify and create indexes to improve query performance.

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -5,12 +5,26 @@ USE airbnb_db;
 
 -- Create indexes on commonly queried columns for optimization
 CREATE INDEX idx_user_email ON User(email);
+
+
 CREATE INDEX idx_property_host ON Property(hostId);
 CREATE INDEX idx_property_location ON Property(location);
+
+
 CREATE INDEX idx_booking_property ON Booking(propertyId);
 CREATE INDEX idx_booking_user ON Booking(userId);
 CREATE INDEX idx_booking_status ON Booking(status);
+
+
 CREATE INDEX idx_payment_booking ON Payment(bookingId);
+
+
+CREATE INDEX idx_review_user ON Review(userId);
+CREATE INDEX idx_review_property ON Review(propertyId);
+
+
+CREATE INDEX idx_sender_message ON Message(senderId);
+CREATE INDEX idx_receiver_message ON Message(receiverId);
 
 
 EXPLAIN ANALYZE SELECT * FROM Booking b
